ecb5bd906f47b30ecba0450cc43ae7545195f139
Connie Zhang <zqq.bnu@gmail.com>
2021-07-20 10:50:55 -0700

    Upload UserRepository.java

---
+++ src/main/java/edu/northeastern/minione/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package edu.northeastern.minione.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.northeastern.minione.model.User;
+
+/**
+ * Create the interface UserRepository. 
+ * 	Note that you will not provide any implementation for it. 
+ * 	Spring Data JPA will implement it for you. 
+ * 	This is part of the "magic" behind the "Spring Data" framework.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByUserName(String userName);
+
+}
