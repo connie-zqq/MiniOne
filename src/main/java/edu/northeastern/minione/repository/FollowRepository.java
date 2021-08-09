84bd86b55363a2a5879b2bd3f49a92c4257d29aa
Connie Zhang <zqq.bnu@gmail.com>
2021-08-08 21:12:35 -0700

    Add Follow Repository

--- src/main/java/edu/northeastern/minione/repository/FollowRepository.java
+++ src/main/java/edu/northeastern/minione/repository/FollowRepository.java
@@ -1,14 +1,18 @@
 package edu.northeastern.minione.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import edu.northeastern.minione.model.Follow;
 
 /**
- * Create the interface SpaceRepository.
+ * Create the interface FollowRepository.
  */
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
+    // In that case the method name below creates property traversal x.followedSpace.id ("Property Expression")
+    List<Follow> findAllByFollowedSpace_Id(Long id);
 }
