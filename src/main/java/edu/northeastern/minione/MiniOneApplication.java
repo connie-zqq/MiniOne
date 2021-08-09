d39415397cfe00e692d087b6b406263df3cd0924
Connie Zhang <zqq.bnu@gmail.com>
2021-08-08 21:06:23 -0700

    Update MiniOneApplication.java - Set the TimeZone

--- src/main/java/edu/northeastern/minione/MiniOneApplication.java
+++ src/main/java/edu/northeastern/minione/MiniOneApplication.java
@@ -1,11 +1,20 @@
 package edu.northeastern.minione;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MiniOneApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MiniOneApplication.class, args);
-	}
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("PST"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniOneApplication.class, args);
+    }
 }
