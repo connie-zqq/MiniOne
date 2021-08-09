8adb5e1372f6baaf082d291726cfbbc308608404
Connie Zhang <zqq.bnu@gmail.com>
2021-08-08 21:47:07 -0700

    Add findAllBySpaceId method

--- src/main/java/edu/northeastern/minione/repository/MomentRepository.java
+++ src/main/java/edu/northeastern/minione/repository/MomentRepository.java
@@ -1,14 +1,18 @@
 package edu.northeastern.minione.repository;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import edu.northeastern.minione.model.Moment;
 
 /**
- * Create the interface SpaceRepository.
+ * Create the interface MomentRepository.
  */
 @Repository
 public interface MomentRepository extends JpaRepository<Moment, Long> {
 
+    Page<Moment> findAllBySpaceId(Long spaceId, Pageable pageable);
 }
