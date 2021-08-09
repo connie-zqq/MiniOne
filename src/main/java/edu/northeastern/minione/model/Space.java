84ab7a7be03c5b84e68455050953e4561c70e33b
Connie Zhang <zqq.bnu@gmail.com>
2021-08-08 21:56:28 -0700

    Add Info about Baby - babyBirthday, babyGender

--- src/main/java/edu/northeastern/minione/model/Space.java
+++ src/main/java/edu/northeastern/minione/model/Space.java
@@ -1,11 +1,17 @@
 package edu.northeastern.minione.model;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,6 +21,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
 
 /**
  * The Space model defines the structure/format in which the data has to be stored in the database "spaces".
@@ -31,11 +38,24 @@ public class Space {
     @JoinColumn(name = "owner_id", referencedColumnName = "id")
     private User owner;
 
-    @OneToMany(mappedBy = "followedSpace")
+    @OneToMany(mappedBy = "followedSpace", fetch = FetchType.LAZY)
     Set<Follow> follows = new HashSet<>();
 
     @Column(name = "space_name", nullable = false)
     private String spaceName;
+
+    @Column(name = "baby_name", nullable = false)
+    private String babyName;
+
+    @Column(name = "baby_gender")
+    private String babyGender;
+
+    @Column(name = "baby_birthday")
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private Date babyBirthday;
+
+    @Column(name = "baby_residence")
+    private String babyResidence;
 
     @Column(name = "space_description")
     private String spaceDescription;
@@ -43,6 +63,9 @@ public class Space {
     @Column(name = "created_date_time")
     @CreationTimestamp
     private Date createdDateTime;
+
+    // Todo: baby birthday
+    // Todo: height, weight
 
     public Space() {
 
@@ -59,8 +82,16 @@ public class Space {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public User getOwner() {
         return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
     }
 
     public Set<Follow> getFollows() {
@@ -75,32 +106,76 @@ public class Space {
         return spaceName;
     }
 
-    public String getSpaceDescription() {
-        return spaceDescription;
-    }
-
-    public Date getCreatedDateTime() {
-        return createdDateTime;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setOwner(User owner) {
-        this.owner = owner;
-    }
-
     public void setSpaceName(String spaceName) {
         this.spaceName = spaceName;
+    }
+
+    public String getBabyName() {
+        return babyName;
+    }
+
+    public void setBabyName(String babyName) {
+        this.babyName = babyName;
+    }
+
+    public String getBabyGender() {
+        return babyGender;
+    }
+
+    public void setBabyGender(String babyGender) {
+        this.babyGender = babyGender;
+    }
+
+    public Date getBabyBirthday() {
+        return babyBirthday;
+    }
+
+    public void setBabyBirthday(Date babyBirthday) {
+        this.babyBirthday = babyBirthday;
+    }
+
+    public String getBabyResidence() {
+        return babyResidence;
+    }
+
+    public void setBabyResidence(String babyResidence) {
+        this.babyResidence = babyResidence;
+    }
+
+    public String getSpaceDescription() {
+        return spaceDescription;
     }
 
     public void setSpaceDescription(String spaceDescription) {
         this.spaceDescription = spaceDescription;
     }
 
+    public Date getCreatedDateTime() {
+        return createdDateTime;
+    }
+
     public void setCreatedDateTime(Date createdDateTime) {
         this.createdDateTime = createdDateTime;
+    }
+
+    public String claculateBabyAge(Date babyBirthday) {
+
+        //Converting obtained Date object to LocalDate object
+        Instant instant = this.babyBirthday.toInstant();
+        ZonedDateTime zone = instant.atZone(ZoneId.systemDefault());
+        LocalDate givenDate = zone.toLocalDate();
+
+        //Calculating the difference between given date to current date.
+        Period period = Period.between(givenDate, LocalDate.now());
+        // eg. 3D
+        if (period.getYears() == 0 && period.getMonths() == 0) {
+            return period.getDays()+"D";
+        }
+        // eg. "2M20D"
+        if (period.getYears() == 0) {
+            return period.getMonths()+"M"+period.getDays()+"D";
+        }
+        return period.getYears()+"Y"+period.getMonths()+"M"+period.getDays()+"D";
     }
 
     @Override
